9d65380df60681d4b30ebce91153690d9331b728
L14X15W06 <laderatheo@gmail.com>
2024-04-15 17:52:18 +0800

    added random things

---
+++ src/main/java/com/example/csit228f2_2/DatabaseCollection.java
@@ -0,0 +1,28 @@
+package com.example.csit228f2_2;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+
+public class DatabaseCollection {
+//    public void MySQLConnection(){
+//        final String URL = "jdbc:mysql://localhost:3306/javadb";
+//        final String USERNAME = "sub";
+//        final String PASSWORD = "pass";
+//        public static Connection getConnection(){
+//            Connection c = null;
+//            try{
+//                Class.forName("com.mysql.cj.jdbc.Driver");
+//                c = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+//                System.out.println("Connected to the Database!");
+//            }catch (ClassNotFoundException | SQLException e){
+//                throw new RuntimeException(e);
+//            }
+//            return c;
+//        }
+//
+//        public static void main(String[] args) {
+//            getConnection();
+//        }
+//    }
+}
